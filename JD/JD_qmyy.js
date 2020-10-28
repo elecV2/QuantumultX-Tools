ca6dbb3a3ce8a2dc8ed3207e472d2c459733db5f
elecV2 <kucoin@icloud.com>
2020-10-28 08:46:54 +0800

    修复可能无法获取cookie的问题

--- JD/JD_qmyy.js
+++ JD/JD_qmyy.js
@@ -7,7 +7,7 @@
 
 hostname = bunearth.m.jd.com, api.m.jd.com
 
-^https:\/\/api\.m\.jd\.com url request-header (\r\n)User-Agent:.+(\r\n) request-header $1User-Agent: jdapp;android;8.4.2;8.0.0;;network/wifi;model/Mi Note 2;osVer/26;appBuild/71043;psn/|7;psq/1;uid/;adk/;ads/;pap/JA2015_311210|8.4.2|ANDROID 8.0.0;osv/8.0.0;pv/2.23;jdv/;ref/com.jingdong.app.mall.WebActivity;partner/huawei;apprpd/Home_Main;Mozilla/5.0 (Linux; Android 8.0.0; Mi Note 2 Build/OPR1.170623.032; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.99 Mobile Safari/537.36$2
+^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=(?!signBean) url request-header (\r\n)User-Agent:.+(\r\n) request-header $1User-Agent: jdapp;android;8.4.2;8.0.0;;network/wifi;model/Mi Note 2;osVer/26;appBuild/71043;psn/|7;psq/1;uid/;adk/;ads/;pap/JA2015_311210|8.4.2|ANDROID 8.0.0;osv/8.0.0;pv/2.23;jdv/;ref/com.jingdong.app.mall.WebActivity;partner/huawei;apprpd/Home_Main;Mozilla/5.0 (Linux; Android 8.0.0; Mi Note 2 Build/OPR1.170623.032; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.99 Mobile Safari/537.36$2
 
 ^https:\/\/bunearth\.m\.jd\.com\/babelDiy\/Zeus url script-response-body https://raw.githubusercontent.com/elecV2/QuantumultX-Tools/master/JD/JD_qmyy.js
 
