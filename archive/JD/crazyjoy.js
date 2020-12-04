a2997f1702d2a160f0a18cd8db8ffb9f018d6038
elecV2 <kucoin@icloud.com>
2020-12-04 10:25:40 +0800

    crazy joy

--- archive/JD/crazyjoy.js
+++ archive/JD/crazyjoy.js
@@ -2,9 +2,10 @@
  * 名称：crazyjoy.js
  * 原执行代码作者：小赤佬
  * 
- * 活动地址: openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://crazy-joy.jd.com/?userInviteCode=4xNkxulet0-rfc3eWGgXhA==%22%20%7D 
- * 复制链接到手机默认浏览器中打开，然后会跳转到京东 APP 中的活动页面。 (链接包含邀请码，介意则手动删除)。
+ * 活动地址: openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://crazy-joy.jd.com/%23/?userInviteCode=4xNkxulet0-rfc3eWGgXhA==%22%20%7D
+ * 复制链接保存到 Notes 中，可直接点击跳转到京东 APP 中的活动页面，或者用默认浏览器打开。 (链接包含邀请码，介意则删除。也可以首次进入后查找自己的邀请码进行替换。 userInviteCode=邀请码%22%20%7D)。
  * 任务执行时下方 JOY 并不会实时移动，以金币变化为准，或手动刷新查看。
+ * 每天首次打开页面会获得较多金币及升级，之后保持正常水平。
  ********  ********
 QUANX rewrite 复写订阅：
 https://raw.githubusercontent.com/elecV2/QuantumultX-Tools/master/archive/JD/crazyjoy.conf
