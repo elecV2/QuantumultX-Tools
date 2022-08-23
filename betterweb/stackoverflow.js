312c64d654280897971f44a6ed2ae0a539bc866e
elecV2 <kucoin@icloud.com>
2022-08-23 09:18:16 +0800

    feat: stackoverflow 网页显示优化

---
+++ betterweb/stackoverflow.js
@@ -0,0 +1,30 @@
+/**
+ * 名称: stackoverflow.js
+ * 说明: stackoverflow 网页显示优化
+ * 制作: https://t.me/elecV2
+ * 更新: 2022-08-22 19:56
+ **********
+[rewrite]
+// 一键订阅: https://raw.githubusercontent.com/elecV2/QuantumultX-Tools/master/betterweb/stackoverflow.conf
+
+// 或者手动添加:
+// 注入 CSS 进行优化
+^https:\/\/stackoverflow\.com\/question url script-response-body https://raw.githubusercontent.com/elecV2/QuantumultX-Tools/master/betterweb/stackoverflow.js
+
+[mitm]
+stackoverflow.com
+**/
+
+if (typeof $response !== 'undefined') {
+  let body = $response.body
+
+  if (/<\/head>/.test(body)) {
+    body = body.replace(/<\/head>/, `<style type="text/css">.pl8, #left-sidebar, #sidebar, #js-gdpr-consent-banner, .bs-sm, .ws4, .js-consent-banner,.ml12,.post-taglist,.js-post-menu,.postcell .mt24,.-flair span:not(.reputation-score),.ai-end,.2vcele,.gs8>.mr16,.gsx>.d-flex{display: none !important}#content {padding: 3px 6px !important;border: none !important;}#mainbar{width: 100% !important}.votecell.post-layout--left {position: absolute;right: 1em;padding-right: 0px !important;opacity: .6;}.post-layout--left:hover {opacity: 1;}.user-info {display: flex;padding-bottom: 0 !important;}.user-action-time {min-width: 272px;white-space: nowrap;z-index: 0;margin-top: 4px !important;}.relativetime {font-size: 22px;color: #FAFAFD;}.comments.js-comments-container,.postcell .mb16,.answercell .mt24,.post-layout--right,.gs8.gsy{margin: 0 !important;padding: 0 !important;}.post-layout {border: 1px solid #003153;border-radius: 6px;overflow: hidden;}.js-post-body {padding: 6px;}.post-signature.flex--item {width: 380px;max-width: 100%;height: 42px;background: #003153;border-radius: 0px;margin-bottom: 0 !important;}.comment-actions {width: 22px !important;}.comment-score {width: 22px !important;padding-right: 0 !important;text-align: right;}.gs8{justify-content: space-around !important;}.user-details{white-space: nowrap;text-overflow: ellipsis;overflow: hidden;}.new-login-form {max-width: 100%;margin: 0;}pre code {white-space: pre-wrap;}</style></head>`)
+
+    // console.log('添加 tamperJS: stackoverflow.js')
+  }
+  $done({ body })
+} else {
+  console.log('stackoverflow.js 用于重写 rewrite 规则，请勿直接运行该脚本\nBY: https://t.me/elecV2')
+  $done()
+}
--- shortcuts/readme.md
+++ shortcuts/readme.md
@@ -1,16 +1,25 @@
-# 一些捷径 
+# 一些捷径
 
-更新频道：https://t.me/elecV2
+适用于 iOS Shortcuts
+更新频道: https://t.me/elecV2
 
 ## JS 直链下载 - 自定义JS直链下载列表，可选择下载全部（默认）或挑选部分进行下载
 
-链接：https://www.icloud.com/shortcuts/50d7d8576dd74f7685811502ff72c078
+链接: https://www.icloud.com/shortcuts/50d7d8576dd74f7685811502ff72c078
 
-说明：
+说明:
+
 - 下载文件保存在 iCoud Drive/shortcuts/elecV2JS文件夹（每次运行都会清空旧的文件）
 - 下载完成后，需手动复制一下文件到相关软件的对应目录
 - 理论上可以下载任何文本文件（.conf/.list/.md等）
 
-## tamperJS  -  配合 MITM 软件，在任意网页中使用 tampermonkey 脚本
+## tamperJS - 配合 MITM 软件，在任意网页中使用 tampermonkey 脚本
 
-链接： https://www.icloud.com/shortcuts/7a21b797d6c34a59a3978683dabc6298
+*iOS15 后 safari 浏览器已实现原生支持，此捷径没有再使用的必要*
+
+链接: https://www.icloud.com/shortcuts/7a21b797d6c34a59a3978683dabc6298
+
+## 京东
+
+- 京东活动列表 https://www.icloud.com/shortcuts/31878ea1e268423ca9bd8d46a1bf26d1
+- 通过京东 APP 打开某个链接: https://www.icloud.com/shortcuts/0cc89f3de2e349eb993d7f28a21603e6
